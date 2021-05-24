2272b2a3f93f9d6b1e5eca2de4b630554c334330
Lukas Glod <lukas.glod@siemens-healthineers.com>
2021-05-24 08:51:32 +0200

    Update Program.cs

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("My name is Lukas");
+            Console.WriteLine("This is branch_1.");
         }
     }
 }
